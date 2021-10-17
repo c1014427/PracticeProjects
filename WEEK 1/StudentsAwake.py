ccceafb13ca5142bd056cb18be98f309092946bb
Travis <c1014427@newcastle.ac.uk>
2021-10-17 20:22:55 +0100

    Beginning of Hello World project
    Understood how to use variables and added a new folder to store all content into its own weeks.

---
+++ WEEK 1/StudentsAwake.py
@@ -0,0 +1,9 @@
+students_in_room = 20
+students_awake = 10
+students_listening = 2
+
+print("Percentage of students awake is:",
+      (students_awake/students_in_room * 100))
+
+print("Percentage of students listening is:",
+      (students_listening/students_in_room * 100))
