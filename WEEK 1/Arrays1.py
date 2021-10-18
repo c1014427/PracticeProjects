aadb43527c4e91ca6964b62df994573419591528
Travis <c1014427@newcastle.ac.uk>
2021-10-18 16:58:46 +0100

    Beginning of Hello World project
    Understood how to use variables and added a new folder to store all content into its own weeks.
    
    Updated the README.md
    
    Added and understood how using Arrays can help with using Lists

---
+++ WEEK 1/Arrays1.py
@@ -0,0 +1,13 @@
+## Status: Shell
+students = ["John", "Paul", "George", "Ringo"]
+
+## Access Elements
+print(students[0])
+print(students[3])
+
+## From the End
+print(students[-2])
+
+## Slicing
+print(students[1:3])
+print(students[1:-1])
