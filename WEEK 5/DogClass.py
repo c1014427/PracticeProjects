77d8d46756b68c77bc8fa9a8bfaa4e9af008abc7
Travis <c1014427@newcastle.ac.uk>
2021-10-27 13:32:15 +0100

    Beginning of Hello World project
    Understood how to use variables and added a new folder to store all content into its own weeks.
    
    Updated the README.md
    
    Added and understood how using Arrays can help with using Lists
    
    Added more type of lists and understood the len() function.
    
    Practiced using Recursion in Python
    
    Started the Week 5 material at created a Person Class
    
    Added the Dog Class

---
+++ WEEK 5/DogClass.py
@@ -0,0 +1,18 @@
+class Dog:
+
+    legs = 4
+
+    def __init__(self, breed, color):
+        self.breed = breed
+        self.color = color
+
+    def speak(self, sound):
+        return f"The {self.color} {self.breed} says: {sound}"
+
+if __name__ == "__main__":
+    d1 = Dog("Husky", "White")
+    d2 = Dog("Bulldog", "Brown")
+
+
+    print(d1.speak("Hello woof woof Everyone!"))
+    print(d2.speak("Where is my treat?"))
